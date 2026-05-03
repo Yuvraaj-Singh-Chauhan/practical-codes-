f231dc2b8d4bad8da3f323bf6514bb246d69527e
Yuvraaj-Singh-Chauhan <chauhanyuvraaj7@gmail.com>
2026-05-03 20:09:19 +0530

    added gauss seidel method

---
+++ gauss_seidel.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double A[3][3]={{10,-1,2},{-1,11,-1},{2,-1,10}};
+    double b[3]={6,25,-11}, x[3]={0,0,0};
+
+    for(int k=0;k<50;k++){
+        for(int i=0;i<3;i++){
+            double sum=0;
+            for(int j=0;j<3;j++) if(j!=i) sum+=A[i][j]*x[j];
+            x[i]=(b[i]-sum)/A[i][i];
+        }
+    }
+
+    for(int i=0;i<3;i++) cout<<x[i]<<" ";
+}
