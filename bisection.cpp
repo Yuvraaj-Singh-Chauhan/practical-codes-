f4f15f03e1dde2ec9c3f2a21ba63f8d7751eabf9
Yuvraaj-Singh-Chauhan <chauhanyuvraaj7@gmail.com>
2026-05-03 20:02:07 +0530

    added bisection method

---
+++ bisection.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double f(double x){ return x*x*x - x - 2; }
+
+int main(){
+    double a=1, b=2, c;
+    for(int i=0;i<100;i++){
+        c=(a+b)/2;
+        if(fabs(f(c))<1e-6) break;
+        if(f(a)*f(c)<0) b=c;
+        else a=c;
+    }
+    cout<<c;
+}
